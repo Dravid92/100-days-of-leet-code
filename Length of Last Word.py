b62bb0f8cae25e0e1be9e4298901e165c0b00837
Mahendran <chandran.poorna1@gmail.com>
2020-07-20 23:13:00 +0530

    Length of Last Word

---
+++ Length of Last Word.py
@@ -0,0 +1,38 @@
+# Runtime: 44 ms, faster than 19.89% of Python3 online submissions for Length of Last Word.
+# Memory Usage: 13.9 MB, less than 38.90% of Python3 online submissions for Length of Last Word.
+
+
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        lst = s.split()
+        
+        if len(lst) >=1:
+            fl = len(lst[-1])
+        else:
+            fl = 0
+        
+        return fl
+
+
+
+"""
+Problem Statement
+"""
+
+"""
+
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+
+"""
